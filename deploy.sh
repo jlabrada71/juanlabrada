e0578faed839a0e3745695260f966afb79afd5f4
Juan Labrada <agile@juanlabrada.com>
2026-07-17 21:40:46 -0300

    feature: implement new deploy system

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+PREV_TAG=$(git rev-parse production/current)
+# bugfix, secfix, refactor, docs, config changes: are patch changes.
+# feature: are minor changes
+# breaking: are major changes
+CHANGE=patch
+if git log $PREV_TAG..HEAD --grep="feature:" -i | grep -q .; then
+  CHANGE=minor
+fi
+if git log $PREV_TAG..HEAD --grep="breaking:" -i | grep -q .; then
+  CHANGE=major 
+fi
+# delete previous production current
+git tag -d production/current
+git push production --delete production/current
+# set the new release version number
+npm version $CHANGE
+VERSION_TAG=$(node -p -e "require('./package.json').version")
+git tag -a production/current -m "Release version $VERSION_TAG"
+
+#push
+echo "Release version $VERSION_TAG"
+git push production production/current
