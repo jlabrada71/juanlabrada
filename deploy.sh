ad48fdf57cc1e149fae87af8e7462195100b07eb
Juan Labrada <agile@juanlabrada.com>
2026-07-17 22:45:28 -0300

    feature: new deploy process

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
+rm .output -rf
+npm run build
+git add .output
+git commit -m "config: preparing release"
 PREV_TAG=$(git rev-parse production/current)
 # bugfix, secfix, refactor, docs, config changes: are patch changes.
 # feature: are minor changes
 # breaking: are major changes
 CHANGE=patch
+NOTES=$(git log $PREV_TAG..HEAD)
 if git log $PREV_TAG..HEAD --grep="feature:" -i | grep -q .; then
   CHANGE=minor
 fi
@@ -19,4 +24,9 @@ git tag -a production/current -m "Release version $VERSION_TAG"
 
 #push
 echo "Release version $VERSION_TAG"
+echo "=================================" >> RELEASE_NOTES.txt
+echo "Release version $VERSION_TAG" >> RELEASE_NOTES.txt
+echo "------------------------------" >> RELEASE_NOTES.txt
+echo $NOTES >> RELEASE_NOTES.txt
+
 git push production production/current
