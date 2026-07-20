caf87b982d6ea0ad77839758194a8134579cacd7
Juan Labrada <agile@juanlabrada.com>
2026-07-20 15:15:36 -0300

    feature: commit release notes before the production TAG

--- deploy-release-notes.sh
+++ deploy-release-notes.sh
@@ -1,6 +1,14 @@
 # get release notes
 PREV_TAG=$(git rev-parse production/current)
+
+# create release notes
+VERSION_TAG=$(node -p -e "require('./package.json').version")
 NOTES=$(git log $PREV_TAG..HEAD)
+echo "Release version $VERSION_TAG"
+echo "=================================" >> RELEASE_NOTES.txt
+echo "Release version $VERSION_TAG" >> RELEASE_NOTES.txt
+echo "------------------------------" >> RELEASE_NOTES.txt
+echo $NOTES >> RELEASE_NOTES.txt
 
 git add .output
 git commit -m "config: preparing release"
@@ -14,10 +22,5 @@ VERSION_TAG=$(node -p -e "require('./package.json').version")
 git tag -a production/current -m "Release version $VERSION_TAG"
 
 #push
-echo "Release version $VERSION_TAG"
-echo "=================================" >> RELEASE_NOTES.txt
-echo "Release version $VERSION_TAG" >> RELEASE_NOTES.txt
-echo "------------------------------" >> RELEASE_NOTES.txt
-echo $NOTES >> RELEASE_NOTES.txt
 git push production production/current
 
