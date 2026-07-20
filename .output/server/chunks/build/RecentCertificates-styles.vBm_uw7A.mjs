e60dc57778499f809ea23837b0e29547866dff0c
Juan Labrada <agile@juanlabrada.com>
2026-07-20 15:34:44 -0300

    config: remove old build

--- .output/server/chunks/build/RecentCertificates-styles.vBm_uw7A.mjs
+++
@@ -1,8 +0,0 @@
-const RecentCertificates_vue_vue_type_style_index_0_scoped_186827be_lang = ".no-scrollbar[data-v-186827be]{scrollbar-width:none;-ms-overflow-style:none}.no-scrollbar[data-v-186827be]::-webkit-scrollbar{display:none}";
-
-const RecentCertificatesStyles_vBm_uw7A = [
-  RecentCertificates_vue_vue_type_style_index_0_scoped_186827be_lang
-];
-
-export { RecentCertificatesStyles_vBm_uw7A as default };
-//# sourceMappingURL=RecentCertificates-styles.vBm_uw7A.mjs.map
--- .output/server/chunks/build/products-styles.4PSNQEi1.mjs
+++
@@ -1,8 +0,0 @@
-const products_vue_vue_type_style_index_0_scoped_664c2a68_lang = "[data-v-664c2a68]{box-sizing:border-box;font-family:Poppins,sans-serif;margin:0;padding:0}.icon[data-v-664c2a68]{--white-shade-1:#fff;--white-rgba:hsla(0,0%,100%,.12);background-color:var(--white-rgba);border-radius:8px;color:var(--white-shade-1);display:grid;font-size:25px;height:65px;place-items:center;text-decoration:none;width:65px}.icon[data-v-664c2a68]:hover{--theme-color-1:#0084ff;--white-shade-1:#fff;background-color:var(--white-shade-1);color:var(--theme-color-1);transform:translateY(-10px)}.background[data-v-664c2a68]{background-image:url(" + __buildAssetsURL("background-1.7KPBTySk.jpg") + ")}";
-
-const productsStyles_4PSNQEi1 = [
-  products_vue_vue_type_style_index_0_scoped_664c2a68_lang
-];
-
-export { productsStyles_4PSNQEi1 as default };
-//# sourceMappingURL=products-styles.4PSNQEi1.mjs.map
