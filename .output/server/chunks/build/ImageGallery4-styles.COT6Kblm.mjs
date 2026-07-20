a26a350b47722ca761b5c5411d379e2c7389d4ff
Juan Labrada <agile@juanlabrada.com>
2026-07-20 15:10:37 -0300

    refactor: removing .output

--- .output/server/chunks/build/ImageGallery4-styles.COT6Kblm.mjs
+++
@@ -1,8 +0,0 @@
-const ImageGallery4_vue_vue_type_style_index_0_scoped_18f97a26_lang = "[data-v-18f97a26]:root{--borderColor:#03a9f3}.gallery[data-v-18f97a26]{--n:3;--m:4;--g:10px;--f:1.2;display:grid;gap:var(--g);grid-template-columns:repeat(var(--m),auto);height:300px;width:250px}.gallery>img[data-v-18f97a26]{cursor:pointer;filter:grayscale(80%);height:0;min-height:100%;min-width:100%;-o-object-fit:cover;object-fit:cover;transition:.35s linear;width:0}.gallery img[data-v-18f97a26]:hover{filter:grayscale(0);height:calc(300px*var(--f)/var(--m));width:calc(250px*var(--f)/var(--n))}";
-
-const ImageGallery4Styles_COT6Kblm = [
-  ImageGallery4_vue_vue_type_style_index_0_scoped_18f97a26_lang
-];
-
-export { ImageGallery4Styles_COT6Kblm as default };
-//# sourceMappingURL=ImageGallery4-styles.COT6Kblm.mjs.map
